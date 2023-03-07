c66b9bcc1f6f8f827cb0d44ee4cc5c59c2e6cfa9
x-Dmitriy Dubovik <x-dmitriy.dubovik@coyote.com>
2023-03-07 17:29:20 +0200

    Facade and Flyweight

---
+++ Patterns/Flyweight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+class FlyweightFactory
+{
+	Hashtable flyweights = new Hashtable();
+	public FlyweightFactory()
+	{
+		flyweights.Add("X", new ConcreteFlyweight());
+		flyweights.Add("Y", new ConcreteFlyweight());
+		flyweights.Add("Z", new ConcreteFlyweight());
+	}
+	public Flyweight GetFlyweight(string key)
+	{
+		if (!flyweights.ContainsKey(key))
+			flyweights.Add(key, new ConcreteFlyweight());
+		return flyweights[key] as Flyweight;
+	}
+}
+
+abstract class Flyweight
+{
+	public abstract void Operation(int extrinsicState);
+}
+
+class ConcreteFlyweight : Flyweight
+{
+	int intrinsicState;
+	public override void Operation(int extrinsicState)
+	{
+		Console.WriteLine("ConcreteFlyweight. extrinsicState = {0}", extrinsicState);
+	}
+}
+
+class UnsharedConcreteFlyweight : Flyweight
+{
+	int allState;
+	public override void Operation(int extrinsicState)
+	{
+		allState = extrinsicState;
+		Console.WriteLine("UnsharedConcreteFlyweight. allState = {0}", allState);
+	}
+}
--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -96,6 +96,37 @@
 			// Wait for user
 			Console.ReadKey();
 			Console.WriteLine();
+
+			//Composite
+			Console.WriteLine("Facade");
+			Facade facade = new Facade();
+			facade.MethodA();
+			facade.MethodB();
+			// Wait for user
+			Console.ReadKey();
+			Console.WriteLine();
+
+			//Flyweight
+			Console.WriteLine("Flyweight");
+			int extrinsicstate = 22;
+
+			FlyweightFactory f = new FlyweightFactory();
+
+			Flyweight fx = f.GetFlyweight("X");
+			fx.Operation(--extrinsicstate);
+
+			Flyweight fy = f.GetFlyweight("Y");
+			fy.Operation(--extrinsicstate);
+
+			Flyweight fd = f.GetFlyweight("D");
+			fd.Operation(--extrinsicstate);
+
+			UnsharedConcreteFlyweight uf = new UnsharedConcreteFlyweight();
+
+			uf.Operation(--extrinsicstate);
+			// Wait for user
+			Console.ReadKey();
+			Console.WriteLine();
 		}
 
 		abstract class Product
